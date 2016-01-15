886441837b895497f12467eea1980b7cb4ad41f9
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2016-01-14 22:30:04 -0800

    Should fix production encryption constant second try

--- config/initializers/api.rb
+++ config/initializers/api.rb
@@ -2,5 +2,5 @@ API_KEY = ENV["API_KEY"]
 NATION_SLUG = ENV["NATION_SLUG"]
 
 if Rails.env.production?
-	API_ENCRYPTION = ENV["API_ENCRYPTION"]
+	API_ENCRYPTION = JSON.parse ENV["API_ENCRYPTION"].gsub('=>', ':')
 end
